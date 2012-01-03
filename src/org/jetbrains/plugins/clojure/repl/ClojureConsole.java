c9da79f0433bf4cb8d809568bc82633fa60aa6ca
Colin Fleming <colin@colinfleming.net>
2012-01-03 23:22:11 +0100

    Handle unreadable forms from REPL

--- src/org/jetbrains/plugins/clojure/repl/ClojureConsole.java
+++ src/org/jetbrains/plugins/clojure/repl/ClojureConsole.java
@@ -43,38 +43,36 @@ import org.jetbrains.plugins.clojure.utils.Editors;
 import java.math.BigDecimal;
 import java.util.regex.Pattern;
 
-public class ClojureConsole extends LanguageConsoleImpl
-{
+public class ClojureConsole extends LanguageConsoleImpl {
   public static final TextAttributes NORMAL_TEXT = ConsoleViewContentType.NORMAL_OUTPUT.getAttributes();
   private final ConsoleHistoryModel historyModel;
   private String currentREPLItem = null;
   private int currentREPLOffset = 0;
 
-  public ClojureConsole(Project project, String title, ConsoleHistoryModel historyModel)
-  {
+  public ClojureConsole(Project project, String title, ConsoleHistoryModel historyModel) {
     super(project, title, ClojureFileType.CLOJURE_LANGUAGE);
     this.historyModel = historyModel;
   }
 
-  public void printResponse(Response response, boolean displayResult)
-  {
+  public void printResponse(Response response, boolean displayResult) {
     setTitle(response.namespace());
 
-    if (response.errorOutput() != null)
-    {
+    if (response.errorOutput() != null) {
       printToHistory(response.errorOutput(), ConsoleViewContentType.ERROR_OUTPUT.getAttributes());
     }
-    if (response.stdOutput() != null)
-    {
+    if (response.stdOutput() != null) {
       printToHistory(response.stdOutput(), NORMAL_TEXT);
     }
 
-    if (displayResult)
-    {
-      for (Object value : response.values())
-      {
+    if (displayResult) {
+      for (Object value : response.values()) {
         print("=> ", ConsoleViewContentType.USER_INPUT.getAttributes());
-        printValue(value);
+        if (value instanceof Response.UnreadableForm) {
+          print(((Response.UnreadableForm) value).form.trim(), NORMAL_TEXT);
+        }
+        else {
+          printValue(value);
+        }
         print("\n", NORMAL_TEXT);
       }
     }
@@ -84,31 +82,23 @@ public class ClojureConsole extends LanguageConsoleImpl
 
   // Based on RT.print
   @SuppressWarnings("HardCodedStringLiteral")
-  private void printValue(Object x)
-  {
+  private void printValue(Object x) {
     // TODO add repl options for printing (*print-meta*, *print-dup*, *print-readably*)
     // TODO then add back meta code
 
-    if (x == null)
-    {
+    if (x == null) {
       print("nil", NORMAL_TEXT);
-    }
-    else if (x instanceof ISeq || x instanceof IPersistentList)
-    {
+    } else if (x instanceof ISeq || x instanceof IPersistentList) {
       print("(", ClojureSyntaxHighlighter.PARENTS);
       printInnerSeq(RT.seq(x));
       print(")", ClojureSyntaxHighlighter.PARENTS);
-    }
-    else if (x instanceof String)
-    {
+    } else if (x instanceof String) {
       CharSequence charSequence = (CharSequence) x;
       StringBuilder buffer = new StringBuilder(charSequence.length() + 8);
       buffer.append('\"');
-      for (int i = 0; i < charSequence.length(); i++)
-      {
+      for (int i = 0; i < charSequence.length(); i++) {
         char c = charSequence.charAt(i);
-        switch (c)
-        {
+        switch (c) {
           case '\n':
             buffer.append("\\n");
             break;
@@ -136,56 +126,41 @@ public class ClojureConsole extends LanguageConsoleImpl
       }
       buffer.append('\"');
       print(buffer.toString(), ClojureSyntaxHighlighter.STRING);
-    }
-    else if (x instanceof IPersistentMap)
-    {
+    } else if (x instanceof IPersistentMap) {
       print("{", ClojureSyntaxHighlighter.BRACES);
-      for (ISeq seq = RT.seq(x); seq != null; seq = seq.next())
-      {
+      for (ISeq seq = RT.seq(x); seq != null; seq = seq.next()) {
         IMapEntry e = (IMapEntry) seq.first();
         printValue(e.key());
         print(" ", NORMAL_TEXT);
         printValue(e.val());
-        if (seq.next() != null)
-        {
+        if (seq.next() != null) {
           print(", ", NORMAL_TEXT);
         }
       }
       print("}", ClojureSyntaxHighlighter.BRACES);
-    }
-    else if (x instanceof IPersistentVector)
-    {
+    } else if (x instanceof IPersistentVector) {
       IPersistentVector vector = (IPersistentVector) x;
       print("[", ClojureSyntaxHighlighter.BRACES);
-      for (int i = 0; i < vector.count(); i++)
-      {
+      for (int i = 0; i < vector.count(); i++) {
         printValue(vector.nth(i));
-        if (i < vector.count() - 1)
-        {
+        if (i < vector.count() - 1) {
           print(" ", NORMAL_TEXT);
         }
       }
       print("]", ClojureSyntaxHighlighter.BRACES);
-    }
-    else if (x instanceof IPersistentSet)
-    {
+    } else if (x instanceof IPersistentSet) {
       print("#{", ClojureSyntaxHighlighter.BRACES);
-      for (ISeq seq = RT.seq(x); seq != null; seq = seq.next())
-      {
+      for (ISeq seq = RT.seq(x); seq != null; seq = seq.next()) {
         printValue(seq.first());
-        if (seq.next() != null)
-        {
+        if (seq.next() != null) {
           print(" ", NORMAL_TEXT);
         }
       }
       print("}", ClojureSyntaxHighlighter.BRACES);
-    }
-    else if (x instanceof Character)
-    {
+    } else if (x instanceof Character) {
       char c = ((Character) x).charValue();
       print("\\", ClojureSyntaxHighlighter.CHAR);
-      switch (c)
-      {
+      switch (c) {
         case '\n':
           print("newline", ClojureSyntaxHighlighter.CHAR);
           break;
@@ -207,73 +182,50 @@ public class ClojureConsole extends LanguageConsoleImpl
         default:
           print(Character.toString(c), ClojureSyntaxHighlighter.CHAR);
       }
-    }
-    else if (x instanceof Class)
-    {
+    } else if (x instanceof Class) {
       print("#=", NORMAL_TEXT);
       print(((Class<?>) x).getName(), NORMAL_TEXT);
-    }
-    else if (x instanceof BigDecimal)
-    {
+    } else if (x instanceof BigDecimal) {
       print(x.toString(), ClojureSyntaxHighlighter.NUMBER);
       print("M", ClojureSyntaxHighlighter.NUMBER);
-    }
-    else if (x instanceof Number)
-    {
+    } else if (x instanceof Number) {
       print(x.toString(), ClojureSyntaxHighlighter.NUMBER);
-    }
-    else if (x instanceof Keyword)
-    {
+    } else if (x instanceof Keyword) {
       print(x.toString(), ClojureSyntaxHighlighter.KEY);
-    }
-    else if (x instanceof Symbol)
-    {
+    } else if (x instanceof Symbol) {
       print(x.toString(), ClojureSyntaxHighlighter.ATOM);
-    }
-    else if (x instanceof Var)
-    {
+    } else if (x instanceof Var) {
       Var v = (Var) x;
       print("#=(var " + v.ns.name + '/' + v.sym + ')', NORMAL_TEXT);
-    }
-    else if (x instanceof Pattern)
-    {
+    } else if (x instanceof Pattern) {
       Pattern p = (Pattern) x;
       print("#\"" + p.pattern() + '\"', NORMAL_TEXT);
-    }
-    else
-    {
+    } else {
       print(x.toString(), NORMAL_TEXT);
     }
   }
 
-  private void printInnerSeq(ISeq x)
-  {
-    for (ISeq seq = x; seq != null; seq = seq.next())
-    {
+  private void printInnerSeq(ISeq x) {
+    for (ISeq seq = x; seq != null; seq = seq.next()) {
       printValue(seq.first());
-      if (seq.next() != null)
-      {
+      if (seq.next() != null) {
         print(" ", NORMAL_TEXT);
       }
     }
   }
 
-  private void print(String text, TextAttributes attributes)
-  {
+  private void print(String text, TextAttributes attributes) {
     printToHistory(text, attributes);
   }
 
-  private void print(String text, TextAttributesKey key)
-  {
+  private void print(String text, TextAttributesKey key) {
     EditorColorsScheme clojureScheme = EditorColorsManager.getInstance().getGlobalScheme();
     printToHistory(text, clojureScheme.getAttributes(key));
   }
 
-  public boolean executeCurrent(boolean immediately)
-  {
+  public boolean executeCurrent(boolean immediately) {
     REPL repl = getConsoleEditor().getUserData(REPL.REPL_KEY);
-    if (repl == null)
-    {
+    if (repl == null) {
       return false;
     }
 
@@ -285,21 +237,18 @@ public class ClojureConsole extends LanguageConsoleImpl
     int offset = caretModel.getOffset();
     String text = document.getText();
 
-    if (!immediately && !"".equals(text.substring(offset).trim()))
-    {
+    if (!immediately && !"".equals(text.substring(offset).trim())) {
       return false;
     }
 
     String candidate = text.trim();
 
-    if (ClojurePsiUtil.isValidClojureExpression(candidate, project) || "".equals(candidate))
-    {
+    if (ClojurePsiUtil.isValidClojureExpression(candidate, project) || "".equals(candidate)) {
       ConsoleHistoryModel consoleHistoryModel = historyModel;
 
       TextRange range = new TextRange(0, document.getTextLength());
       editor.getSelectionModel().setSelection(range.getStartOffset(), range.getEndOffset());
-      if (!StringUtil.isEmptyOrSpaces(candidate))
-      {
+      if (!StringUtil.isEmptyOrSpaces(candidate)) {
         addTextRangeToHistory(getConsoleEditor(), range);
         consoleHistoryModel.addToHistory(candidate);
         Editors.scrollDown(getHistoryViewer());
@@ -316,32 +265,25 @@ public class ClojureConsole extends LanguageConsoleImpl
     return false;
   }
 
-  public void setTitle(final String title)
-  {
+  public void setTitle(final String title) {
     final Content content = getConsoleEditor().getUserData(REPL.CONTENT_KEY);
     final REPL repl = getConsoleEditor().getUserData(REPL.REPL_KEY);
-    if ((content == null) || (repl == null))
-    {
+    if ((content == null) || (repl == null)) {
       return;
     }
 
-    ApplicationManager.getApplication().invokeLater(new Runnable()
-    {
-      public void run()
-      {
+    ApplicationManager.getApplication().invokeLater(new Runnable() {
+      public void run() {
         content.setDisplayName(repl.getType() + ": " + title);
       }
     });
   }
 
   // Copied from LanguageConsoleImpl
-  public String addTextRangeToHistory(final EditorEx editor, final TextRange textRange)
-  {
+  public String addTextRangeToHistory(final EditorEx editor, final TextRange textRange) {
     final Ref<String> ref = Ref.create("");
-    ApplicationManager.getApplication().runWriteAction(new Runnable()
-    {
-      public void run()
-      {
+    ApplicationManager.getApplication().runWriteAction(new Runnable() {
+      public void run() {
         ref.set(addTextRangeToHistoryImpl(editor, textRange));
       }
     });
@@ -349,8 +291,7 @@ public class ClojureConsole extends LanguageConsoleImpl
   }
 
   // Copied from LanguageConsoleImpl
-  private String addTextRangeToHistoryImpl(EditorEx fromEditor, TextRange textRange)
-  {
+  private String addTextRangeToHistoryImpl(EditorEx fromEditor, TextRange textRange) {
     Document history = getHistoryViewer().getDocument();
     MarkupModel markupModel = DocumentMarkupModel.forDocument(history, getProject(), true);
 
@@ -360,8 +301,7 @@ public class ClojureConsole extends LanguageConsoleImpl
     Document fromDocument = fromEditor.getDocument();
     String fullText = fromDocument.getText(textRange);
 
-    for (int line = startLine; line <= endLine; line++)
-    {
+    for (int line = startLine; line <= endLine; line++) {
       int lineStart = fromDocument.getLineStartOffset(line);
       int lineEnd = fromDocument.getLineEndOffset(line);
 
@@ -375,23 +315,20 @@ public class ClojureConsole extends LanguageConsoleImpl
       int localStartOffset = lineRange.getStartOffset();
       HighlighterIterator iterator = fromEditor.getHighlighter().createIterator(localStartOffset);
       int localEndOffset = lineRange.getEndOffset();
-      while (!iterator.atEnd())
-      {
+      while (!iterator.atEnd()) {
         int itStart = iterator.getStart();
-        if (itStart > localEndOffset)
-        {
+        if (itStart > localEndOffset) {
           break;
         }
         int itEnd = iterator.getEnd();
-        if (itEnd >= localStartOffset)
-        {
+        if (itEnd >= localStartOffset) {
           int start = Math.max(itStart, localStartOffset) - localStartOffset + offset;
           int end = Math.min(itEnd, localEndOffset) - localStartOffset + offset;
           markupModel.addRangeHighlighter(start,
-                                          end,
-                                          HighlighterLayer.SYNTAX,
-                                          iterator.getTextAttributes(),
-                                          HighlighterTargetArea.EXACT_RANGE);
+              end,
+              HighlighterLayer.SYNTAX,
+              iterator.getTextAttributes(),
+              HighlighterTargetArea.EXACT_RANGE);
         }
         iterator.advance();
       }
@@ -400,13 +337,12 @@ public class ClojureConsole extends LanguageConsoleImpl
       appendToHistoryDocument(history, "\n");
 
       // Add REPL separators
-      if (ClojureProjectSettings.getInstance(getProject()).separateREPLItems && (line == startLine))
-      {
+      if (ClojureProjectSettings.getInstance(getProject()).separateREPLItems && (line == startLine)) {
         RangeHighlighter marker = markupModel.addRangeHighlighter(offset,
-                                                                  history.getTextLength(),
-                                                                  HighlighterLayer.ADDITIONAL_SYNTAX,
-                                                                  null,
-                                                                  HighlighterTargetArea.EXACT_RANGE);
+            history.getTextLength(),
+            HighlighterLayer.ADDITIONAL_SYNTAX,
+            null,
+            HighlighterTargetArea.EXACT_RANGE);
         EditorColorsScheme clojureScheme = EditorColorsManager.getInstance().getGlobalScheme();
         marker.setLineSeparatorColor(clojureScheme.getColor(CodeInsightColors.METHOD_SEPARATORS_COLOR));
         marker.setLineSeparatorPlacement(SeparatorPlacement.TOP);
@@ -417,44 +353,38 @@ public class ClojureConsole extends LanguageConsoleImpl
   }
 
   // Copied from LanguageConsoleImpl
-  private static void duplicateHighlighters(MarkupModel to, MarkupModel from, int offset, TextRange textRange)
-  {
+  private static void duplicateHighlighters(MarkupModel to, MarkupModel from, int offset, TextRange textRange) {
     EditorColorsScheme clojure = EditorColorsManager.getInstance().getGlobalScheme();
     TextAttributes matched = clojure.getAttributes(CodeInsightColors.MATCHED_BRACE_ATTRIBUTES);
     TextAttributes unmatched = clojure.getAttributes(CodeInsightColors.UNMATCHED_BRACE_ATTRIBUTES);
 
-    for (RangeHighlighter rangeHighlighter : from.getAllHighlighters())
-    {
+    for (RangeHighlighter rangeHighlighter : from.getAllHighlighters()) {
       int localOffset = textRange.getStartOffset();
       int start = Math.max(rangeHighlighter.getStartOffset(), localOffset) - localOffset;
       int end = Math.min(rangeHighlighter.getEndOffset(), textRange.getEndOffset()) - localOffset;
-      if (start > end)
-      {
+      if (start > end) {
         continue;
       }
 
       TextAttributes attributes = rangeHighlighter.getTextAttributes();
-      if (matched.equals(attributes) || unmatched.equals(attributes))
-      {
+      if (matched.equals(attributes) || unmatched.equals(attributes)) {
         continue;
       }
 
       RangeHighlighter h = to.addRangeHighlighter(start + offset,
-                                                  end + offset,
-                                                  rangeHighlighter.getLayer(),
-                                                  attributes,
-                                                  rangeHighlighter.getTargetArea());
+          end + offset,
+          rangeHighlighter.getLayer(),
+          attributes,
+          rangeHighlighter.getTargetArea());
       ((RangeHighlighterEx) h).setAfterEndOfLine(((RangeHighlighterEx) rangeHighlighter).isAfterEndOfLine());
     }
   }
 
-  public ConsoleHistoryModel getHistoryModel()
-  {
+  public ConsoleHistoryModel getHistoryModel() {
     return historyModel;
   }
 
-  public void saveCurrentREPLItem()
-  {
+  public void saveCurrentREPLItem() {
     Editor editor = getCurrentEditor();
     Document document = editor.getDocument();
     CaretModel caretModel = editor.getCaretModel();
@@ -463,8 +393,7 @@ public class ClojureConsole extends LanguageConsoleImpl
   }
 
   // Assumed to be run in a write action
-  public void restoreCurrentREPLItem()
-  {
+  public void restoreCurrentREPLItem() {
     Editor editor = getCurrentEditor();
     Document document = editor.getDocument();
     document.setText(currentREPLItem == null ? "" : currentREPLItem);
--- src/org/jetbrains/plugins/clojure/repl/Response.java
+++ src/org/jetbrains/plugins/clojure/repl/Response.java
@@ -1,11 +1,14 @@
 package org.jetbrains.plugins.clojure.repl;
 
+import clojure.lang.ISeq;
 import clojure.lang.Keyword;
+import clojure.lang.Seqable;
 import clojure.tools.nrepl.Connection;
 import clojure.tools.nrepl.SafeFn;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.plugins.clojure.utils.ClojureUtils;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -23,7 +26,6 @@ public class Response
   @NonNls
   public static final Keyword VALUE_KEYWORD = Keyword.intern("value");
 
-  private static final SafeFn MAP = SafeFn.find(ClojureUtils.CORE_NAMESPACE, "map");
   private static final SafeFn READ_STRING = SafeFn.find(ClojureUtils.CORE_NAMESPACE, "read-string");
 
   private final Connection.Response response;
@@ -40,7 +42,19 @@ public class Response
 
   public final List<Object> values()
   {
-    return (List<Object>) MAP.sInvoke(READ_STRING, combinedResponse().get(VALUE_KEYWORD));
+    List<Object> ret = new ArrayList<Object>();
+    Seqable values = (Seqable) combinedResponse().get(VALUE_KEYWORD);
+    if (values != null) {
+      for (ISeq seq = values.seq(); seq != null; seq = seq.next()) {
+        String next = (String) seq.first();
+        try {
+          ret.add(READ_STRING.sInvoke(next));
+        } catch (Exception ignore) {
+          ret.add(new UnreadableForm(next));
+        }
+      }
+    }
+    return ret;
   }
 
   public final String namespace()
@@ -56,5 +70,13 @@ public class Response
   public final String stdOutput()
   {
     return (String) combinedResponse().get(OUTPUT_KEYWORD);
+  }
+
+  public static class UnreadableForm {
+      public final String form;
+
+      public UnreadableForm(String form) {
+          this.form = form;
+      }
   }
 }
