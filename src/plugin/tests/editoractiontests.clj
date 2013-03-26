038de1107b6d1c21848f5901f73716ece939a55e
Colin Fleming <colin@colinfleming.net>
2013-03-27 12:33:22 +1300

    la-clojure-21 Paredit support Fix tests.

--- src/plugin/tests/editoractiontests.clj
+++ src/plugin/tests/editoractiontests.clj
@@ -13,41 +13,41 @@
 ;                             "(a (b) c d e)")))
 
 (deftest slurp-backwards-tests
-  (is (editor-action-result? "plugin.actions.paredit.slurp-backwards"
+  (is (editor-action-result? ":plugin.actions.paredit/slurp-backwards"
                              "(foo bar (baz<caret> quux) zot)"
                              "(foo (bar baz<caret> quux) zot)"))
-  (is (editor-action-result? "plugin.actions.paredit.slurp-backwards"
+  (is (editor-action-result? ":plugin.actions.paredit/slurp-backwards"
                              "(a b ((c<caret> d)) e f)"
                              "(a (b (c<caret> d)) e f)"))
-  (is (editor-action-result? "plugin.actions.paredit.slurp-backwards"
+  (is (editor-action-result? ":plugin.actions.paredit/slurp-backwards"
                              (test/lines "(a"
                                          " (<caret>b c))")
                              (test/lines "((a"
                                          "   <caret>b c))"))))
 
 (deftest slurp-forwards-tests
-  (is (editor-action-result? "plugin.actions.paredit.slurp-forwards"
+  (is (editor-action-result? ":plugin.actions.paredit/slurp-forwards"
                              "(foo (bar <caret>baz) quux zot)"
                              "(foo (bar <caret>baz quux) zot)"))
-  (is (editor-action-result? "plugin.actions.paredit.slurp-forwards"
+  (is (editor-action-result? ":plugin.actions.paredit/slurp-forwards"
                              "(a b ((c<caret> d)) e f)"
                              "(a b ((c<caret> d) e) f)"))
-  (is (editor-action-result? "plugin.actions.paredit.slurp-forwards"
+  (is (editor-action-result? ":plugin.actions.paredit/slurp-forwards"
                              (test/lines "((a b<caret>)"
                                          " c)")
                              (test/lines "((a b<caret>"
                                          "    c))"))))
 
 (deftest splice-tests
-  (is (editor-action-result? "plugin.actions.paredit.splice"
+  (is (editor-action-result? ":plugin.actions.paredit/splice"
                              "(a (b c <caret>d) e)"
                              "(a b c <caret>d e)"))
-  (is (editor-action-result? "plugin.actions.paredit.splice"
+  (is (editor-action-result? ":plugin.actions.paredit/splice"
                              "(abc (def <caret>ghi))"
                              "(abc def <caret>ghi)"))
-  (is (editor-action-result? "plugin.actions.paredit.splice"
+  (is (editor-action-result? ":plugin.actions.paredit/splice"
                              "((abc <caret>def) ghi)"
                              "(abc <caret>def ghi)"))
-  (is (editor-action-result? "plugin.actions.paredit.splice"
+  (is (editor-action-result? ":plugin.actions.paredit/splice"
                              "((abc <caret>def ghi))"
                              "(abc <caret>def ghi)")))
